ac7923880e79a6eea8700c8d85bf8861e5bc3c63
Maksim Terentiev <maksim_terentiev@mail.ru>
2022-04-26 12:07:39 +0300

    Minor correction main.c: example manual tree building, now with colors

--- main.c
+++ main.c
@@ -6,15 +6,19 @@ int main() {
 
     node_t* node = new_node();
         node->key = 43;
+        node->color=BLACK;
         node->left=new_node();
-	    node->left->parent=node;
+            node->left->parent=node;
             node->left->key = 41;
+            node->left->color=RED;
             node->left->right = new_node();
-	        node->left->right->parent=node->left;
+                node->left->right->parent=node->left;
                 node->left->right->key = 42;
-	node->right = new_node();
-	        node->right->parent=node;
-		node->right->key = 44;
+                node->left->right->color=BLACK;
+    node->right = new_node();
+            node->right->parent=node;
+            node->right->key = 44;
+            node->right->color=RED;
 
     print_tree(node);
     printf("\n"); // for terminal flushing
